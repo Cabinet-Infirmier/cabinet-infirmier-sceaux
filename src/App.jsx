324e2fef26306996c6f74ce2a08af88a0f5bc5cb
Cédric Phung <cedric3011@outlook.fr>
2025-02-22 11:01:51 +0100

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="services" element={<Services />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
