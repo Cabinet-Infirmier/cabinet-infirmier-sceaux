ed4d4445ccfbaab3d679b6801ed04d4ddedc3b19
MaryelN <maryel.navarromtz@gmail.com>
2025-02-23 18:20:07 +0100

    Add Navbar to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Homepage from "./pages/Homepage";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="services" element={<Services />} />
