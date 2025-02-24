ce0cd19916e91cb5bd4322ed07b9e497e904c090
MaryelN <maryel.navarromtz@gmail.com>
2025-02-24 21:39:55 +0100

    Add of min-width & paragraphs to mainInfo

--- src/components/squareInfo/Squareinfo.jsx
+++ src/components/squareInfo/Squareinfo.jsx
@@ -1,24 +1,27 @@
 import styles from "./SquareInfo.module.css";
+import Button from "../button/Button";
 
 export default function SquareInfo() {
   return (
     <div className={styles.squareInfo}>
       <h3 className="pink">A PROPOS DE NOUS</h3>
       <p>
-        Le cabinet se compose de 4 infirmières diplômées d’état réparties en 2
-        tournées, Il se trouve ausein du centre santé sport Robinson, où
+        Le cabinet se compose de 4 infirmières diplômées d&apos;état réparties
+        en 2 tournées, Il se trouve ausein du centre santé sport Robinson, où
         excercent également 3 kinésithérapeutes et 1 pédicure podologue.
-        <br />
-        <b>
-          Les infirmières vous proposent des soins au cabinet et à votre
-          domicile sur rendez-vous. Nous pratiquons le tiers payant sécurité
-          sociale et mutuelle (pas d’avance de frais).
-        </b>
+      </p>
+
+      <b>
+        Les infirmières vous proposent des soins au cabinet et à votre domicile
+        sur rendez-vous. Nous pratiquons le tiers payant sécurité sociale et
+        mutuelle (pas d&apos;avance de frais).
+      </b>
+      <p>
         Dans le cadre de la formation continue, nous effectuons régulièrement
         des formations afin de nous adapter aux nouvelles techniques, protocoles
         de soins et innovations.
       </p>
-      <button>En savoir Plus</button>
+      <Button>En savoir Plus</Button>
     </div>
   );
 }
