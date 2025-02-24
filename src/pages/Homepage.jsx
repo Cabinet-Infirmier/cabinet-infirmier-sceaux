7d0a935c7193f95a3c7ff43dafb65add63e5fd50
MaryelN <maryel.navarromtz@gmail.com>
2025-02-24 21:15:04 +0100

    Add Info banner to Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,30 @@
+import styles from "./Homepage.module.css";
 import Hero from "../components/hero/Hero";
 import SquareInfo from "../components/squareInfo/Squareinfo";
+import iconCalendar from "../assets/icons/icon-calendary.png";
+import iconMoney from "../assets/icons/icon-money.png";
 
 function Homepage() {
   return (
     <div>
       <Hero />
       <SquareInfo />
+      <div className={styles.bannerInfo}>
+        <div>
+          <img src={iconCalendar} alt="Calendrier icon" />
+          <p>Disponibles 7 jours sur 7.</p>
+          <p>
+            Vous pouvez nous contacter par téléphone et SMS tous les jours de
+            l’année.
+          </p>
+        </div>
+        <div>
+          <img src={iconMoney} alt="Paiement icon" />
+          <p>PAIEMENTS ACCEPTÉS</p>
+          <p>Espèces</p>
+          <p>Chèque</p>
+        </div>
+      </div>
     </div>
   );
 }
