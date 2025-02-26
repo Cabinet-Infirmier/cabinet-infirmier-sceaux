95a317db44ee9e77a16fdfa7a4d9a5dc02365b71
Cédric Phung <cedric3011@outlook.fr>
2025-02-26 19:02:44 +0100

    add button and modify style

--- src/components/nurse-team/NurseTeam.jsx
+++ src/components/nurse-team/NurseTeam.jsx
@@ -1,3 +1,4 @@
+import Button from "../button/Button";
 import Nurse from "../nurse/Nurse";
 import styles from "./NurseTeam.module.css";
 
@@ -5,12 +6,13 @@ function NurseTeam() {
   return (
     <section className={styles.nurses}>
       <h2>Infirmières au cabinet</h2>
-      <div className={styles.container}>
+      <ul className={styles.container}>
         <Nurse name="Angelina" fileName="angelina.png" />
         <Nurse name="Angelina" fileName="angelina.png" />
         <Nurse name="Angelina" fileName="angelina.png" />
         <Nurse name="Angelina" fileName="angelina.png" />
-      </div>
+      </ul>
+      <Button>Prendre RDV</Button>
     </section>
   );
 }
--- src/components/nurse/Nurse.jsx
+++ src/components/nurse/Nurse.jsx
@@ -2,10 +2,10 @@ import styles from "./Nurse.module.css";
 
 function Nurse({ name, fileName }) {
   return (
-    <div className={styles.nurse}>
+    <li className={styles.nurse}>
       <img src={`./images/nurses/${fileName}`} alt={`${fileName}`} />
       <p>{name}</p>
-    </div>
+    </li>
   );
 }
 
