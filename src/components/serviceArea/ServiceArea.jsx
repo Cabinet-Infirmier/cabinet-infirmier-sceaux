971b8e2f977a04a567f5ec43de62fe60ceb3d7e6
MaryelN <maryel.navarromtz@gmail.com>
2025-02-28 20:14:04 +0100

    Add of gallery section

--- src/components/serviceArea/ServiceArea.jsx
+++ src/components/serviceArea/ServiceArea.jsx
@@ -1,17 +1,33 @@
 import IconCabinet from "../../assets/icons/icon-cabinet.png";
+import IconAddress from "../../assets/icons/icon-address.png";
 import MapImage from "../../assets/images/sector-map.png";
 import ZoomImage from "../zoomImage/ZoomImage";
 import styles from "../serviceArea/ServiceArea.module.css";
+import CabinetImage from "../../assets/images/cabinet-1.jpg";
+import Button from "../button/Button";
 
 export default function ServiceArea() {
   return (
     <div className={styles.ServiceArea}>
-      <div className={styles.Container}></div>
       <div className={styles.Container}>
-        <div className={styles.MapContainer}>
-          <ZoomImage src={MapImage} alt="Carte de zones de service" />
+        <div className={styles.CabinetContainer}>
+          <img
+            className={styles.CabinetImage}
+            src={CabinetImage}
+            alt="Image du cabinet"
+          />
         </div>
-        <div className={styles.MapTitle}>
+        <div className={styles.ButtonContainer}>
+          <Button>Voir Plus</Button>
+        </div>
+        <div className={styles.Title}>
+          <img src={IconAddress} alt="Cabinet medical icône" />
+          <h4>Secteur d’intervention à domicile</h4>
+        </div>
+      </div>
+      <div className={styles.Container}>
+        <ZoomImage src={MapImage} alt="Carte de zones de service" />
+        <div className={styles.Title}>
           <img src={IconCabinet} alt="Cabinet medical icône" />
           <h4>Secteur d’intervention à domicile</h4>
         </div>
