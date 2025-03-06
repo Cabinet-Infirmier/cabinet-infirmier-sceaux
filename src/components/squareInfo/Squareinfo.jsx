6a29434ce4a6de086b61d072bd30a22a050ce7e9
Cédric Phung <cedric3011@outlook.fr>
2025-03-06 09:33:23 +0100

    modify display

--- src/components/squareInfo/Squareinfo.jsx
+++ src/components/squareInfo/Squareinfo.jsx
@@ -4,24 +4,26 @@ import Button from "../button/Button";
 export default function SquareInfo() {
   return (
     <div className={styles.squareInfo}>
-      <h3>A PROPOS DE NOUS</h3>
-      <p>
-        Le cabinet se compose de 4 infirmières diplômées d&apos;état réparties
-        en 2 tournées, Il se trouve ausein du centre santé sport Robinson, où
-        excercent également 3 kinésithérapeutes et 1 pédicure podologue.
-      </p>
+      <div className={styles.container}>
+        <h3>A PROPOS DE NOUS</h3>
+        <p>
+          Le cabinet se compose de 4 infirmières diplômées d&apos;état réparties
+          en 2 tournées. Il se trouve au sein du centre santé sport Robinson, où
+          excercent également 3 kinésithérapeutes et 1 pédicure podologue.
+        </p>
 
-      <b>
-        Les infirmières vous proposent des soins au cabinet et à votre domicile
-        sur rendez-vous. Nous pratiquons le tiers payant sécurité sociale et
-        mutuelle (pas d&apos;avance de frais).
-      </b>
-      <p>
-        Dans le cadre de la formation continue, nous effectuons régulièrement
-        des formations afin de nous adapter aux nouvelles techniques, protocoles
-        de soins et innovations.
-      </p>
-      <Button>En savoir Plus</Button>
+        <strong>
+          Les infirmières vous proposent des soins au cabinet et à votre
+          domicile sur rendez-vous. Nous pratiquons le tiers payant sécurité
+          sociale et mutuelle (pas d&apos;avance de frais).
+        </strong>
+        <p>
+          Dans le cadre de la formation continue, nous effectuons régulièrement
+          des formations afin de nous adapter aux nouvelles techniques,
+          protocoles de soins et innovations.
+        </p>
+        <Button>En savoir Plus</Button>
+      </div>
     </div>
   );
 }
