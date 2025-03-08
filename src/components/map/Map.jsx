0cea75aab2995c8aee888941dd46e99da708e8dc
MaryelN <maryel.navarromtz@gmail.com>
2025-03-08 16:38:04 +0100

    Creation Map component

---
+++ src/components/map/Map.jsx
@@ -0,0 +1,19 @@
+export default function Map() {
+  return (
+    <div className="map.section">
+      <div className="gmap-frame">
+        <iframe
+          width="400"
+          height="400"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
+          src="https://maps.google.com/maps?width=400&amp;height=400&amp;hl=en&amp;q=18%20square%20robinson%20RDC%20Gauche%20interphone%2015,%2092330%20Sceaux+(Mme%20THOMINOT-%20Mme%20NOEL-%20Mme%20SAINT-JOANNIS%20&amp;%20Mme%20PELTRET)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        >
+          <a href="https://www.gps.ie/collections/drones/">drone quadcopter</a>
+        </iframe>
+      </div>
+    </div>
+  );
+}
