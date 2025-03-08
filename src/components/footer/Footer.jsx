94bb155f6752afd3f862759dc7d7fe736a68e6e1
MaryelN <maryel.navarromtz@gmail.com>
2025-03-08 15:38:45 +0100

    Footer style & icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Footer.module.css";
 import iconLogo from "../../assets/icons/logo-infirmiere.png";
-import iconAddress from "../../assets/icons/icon-address.png";
-import iconPhone from "../../assets/icons/icon-phone.png";
-import iconEmail from "../../assets/icons/icon-email.png";
-import iconSms from "../../assets/icons/icon-sms.png";
 import Button from "../button/Button";
+import {
+  CalendarClock,
+  MapPin,
+  Phone,
+  MessageCircleMore,
+  AtSign,
+} from "lucide-react";
 
 export default function Footer() {
   return (
@@ -14,18 +17,23 @@ export default function Footer() {
         <NavLink to="/">
           <img className={styles.logo} src={iconLogo} alt="Logo Infirmiere" />
         </NavLink>
-        <Button>Contacter</Button>
+        <NavLink to="/contact">
+          <Button>Contacter</Button>
+        </NavLink>
       </div>
       <div className={styles.container}>
         <h3>Horaires</h3>
-        <p>7j/7 de 7h à 17h.</p>
+        <div className={styles.containerHours}>
+          <CalendarClock color="#d78b8b" />
+          <p>7j/7 de 7h à 17h.</p>
+        </div>
         <h3>Liens pratiques</h3>
         <ul>
           <li>
             <NavLink to="/services">Services</NavLink>
           </li>
           <li>
-            <NavLink to="/infirmieres">Nos infirmières</NavLink>
+            <NavLink to="#nurseTeam">Nos infirmières</NavLink>
           </li>
           <li>
             <NavLink to="/contact">Contact</NavLink>
@@ -35,7 +43,7 @@ export default function Footer() {
       <div className={styles.container}>
         <h3>Contact</h3>
         <div className={styles.footerInfo}>
-          <img src={iconAddress} alt="Address Icon" />
+          <MapPin color="#d78b8b" />
           <a href="https://www.google.com/maps/dir//Infirmi%C3%A8res+%C3%A0+domicile:+Mme+THOMINOT-+Mme+NOEL-+Mme+SAINT-JOANNIS+%26+Mme+PELTRET,+18+square+robinson+RDC+Gauche+interphone+15,+92330+Sceaux/@48.779387,2.2796306,17z/data=!3m1!5s0x47e670b27e1edec1:0xf9e118013997a658!4m18!1m8!3m7!1s0x47e670b27f898bfb:0x4a7d1c0fa00bfb97!2sInfirmi%C3%A8res+%C3%A0+domicile:+Mme+THOMINOT-+Mme+NOEL-+Mme+SAINT-JOANNIS+%26+Mme+PELTRET!8m2!3d48.7793781!4d2.2822223!15sCkxDYWJpbmV0IGluZmlybWllcnMgMTggc3F1YXJlIHJvYmluc29uIFJEQyBHYXVjaGUgaW50ZXJwaG9uZSAxNSwgOTIzMzAgU2NlYXV4kgEObnVyc2luZ19hZ2VuY3ngAQA!16s%2Fg%2F1yj4k3nxk!4m8!1m0!1m5!1m1!1s0x47e670b27f898bfb:0x4a7d1c0fa00bfb97!2m2!1d2.2822223!2d48.7793781!3e3?entry=ttu&g_ep=EgoyMDI1MDIxOS4xIKXMDSoASAFQAw%3D%3D">
             Cabinet infirmiers
             <br></br>
@@ -45,15 +53,15 @@ export default function Footer() {
           </a>
         </div>
         <div className={styles.footerInfo}>
-          <img src={iconPhone} alt="Telephone icon" />
+          <Phone color="#d78b8b" />
           <a href="tel:0146600974">01.46.60.09.74</a>
         </div>
         <div className={styles.footerInfo}>
-          <img src={iconSms} alt="Sms icon" />
+          <MessageCircleMore color="#d78b8b" />
           <a href="sms:0749703118">07.49.70.31.18</a>
         </div>
         <div className={styles.footerInfo}>
-          <img src={iconEmail} alt="Email icon" />
+          <AtSign color="#d78b8b" />
           <NavLink to="/contact">cabinet.idel.sceaux@gmail.com</NavLink>
         </div>
       </div>
